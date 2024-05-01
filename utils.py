040bf8d294c5d16196a680cdb11d8d71152fa2c2
mikeleske <42025313+mikeleske@users.noreply.github.com>
2024-05-01 13:06:50 +0200

    update utils.py

--- utils.py
+++ utils.py
@@ -82,3 +82,45 @@ def parse_fasta_file_gtdb(file: str, domain: str, out_file: str = None, max_seq_
         df.to_csv(path_or_buf=out_file, sep=',', index=False)
 
     return df
+
+
+def parse_fasta_file_gtdb_gzip(file: str, domain: str, out_file: str = None, max_seq_length: int = None, V3V4_start: int = None, V3V4_end: int = None, num_records: int = None) -> pd.DataFrame :
+    """
+        tbd
+    """
+
+    rows_list = []
+    columns = ['ID','Taxa','Kingdom','Phylum','Class','Order','Family','Genus','Species','SeqLen','Seq']
+
+    #fasta_file = SeqIO.parse(file, 'fasta')
+
+    with gzip.open(file, "rt") as handle:
+        for rec in SeqIO.parse(handle, "fasta"):
+            if str(rec.description).split()[1].startswith(domain):
+                description = str(rec.description)
+                id = description.split()[0]
+                taxa = description.rsplit(id)[1].split(' [')[0].replace(';', '~').strip()
+                taxa_split = taxa.split('~')
+    
+                if len(taxa_split) == 7:
+                    kingdom, phylum, _class, order, family, genus, species = taxa_split
+                    seq = str(rec.seq)
+                    seq_len = len(seq)
+    
+                    if max_seq_length:
+                        seq = seq[:max_seq_length]
+                    
+                    if V3V4_start:
+                        seq = seq[V3V4_start:V3V4_end]
+                    
+                    dict1 = dict( (col, val) for (col, val) in 
+                                  zip(columns, [id, taxa, kingdom, phylum, _class, order, family, genus, species, seq_len, seq]))
+                    rows_list.append(dict1)
+
+    
+    df = pd.DataFrame(rows_list, columns=columns)
+
+    if out_file:
+        df.to_csv(path_or_buf=out_file, sep=',', index=False)
+
+    return df
