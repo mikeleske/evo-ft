d5e7b52ee6935e8507a8cab3ea4aa84941f82a91
mikeleske <42025313+mikeleske@users.noreply.github.com>
2024-05-06 01:38:01 +0200

    update

--- inference.py
+++ inference.py
@@ -3,6 +3,8 @@ from evo_code.configuration_hyena import StripedHyenaConfig
 from evo_code.modeling_hyena import StripedHyenaModelForCausalLM
 import torch
 
+import pandas as pd
+
 model_id = "togethercomputer/evo-1-131k-base"
 default_config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
 
@@ -15,17 +17,25 @@ model = StripedHyenaModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+#tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
 tokenizer.pad_token = "|"
 tokenizer.eos_token = "~"
+
+
 
 
 def inference(seq):
     #prompt = f"### Seq: {row['Seq']}\n ### Genus: "
     prompt = f"{seq}<G>"
     inputs = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+    print(len(inputs), inputs)
     outputs = model.generate(inputs, max_new_tokens=50)#, do_sample=True, top_k=50, top_p=0.95)
+    print(len(outputs), outputs)
     return str(tokenizer.batch_decode(outputs, skip_special_tokens=True))
 
-seq = ''
-print(inference(seq))
+df = pd.read_csv('r220_16S_bac120_sft.csv', sep=',')
+
+for i in range(10):
+    print('\n', df.loc[i, 'Genus'], '\n')
+    print('Evo FT prediction:\n', inference(df.loc[i, 'SeqV3V4']))
